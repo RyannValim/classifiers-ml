cf12cfbf02bcc5cb7d2fd55f36bec85e52e26294
Ryann Valim <iryanngustavo@gmail.com>
2026-06-19 16:57:01 -0300

    feat: bank e pima finalizados

--- pima/carregamento_dados.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-
-def carregar_dados():
-    return pd.read_csv('../datasets/pima_diabetes.csv', sep=',')
--- pima/salvar_modelo.py
+++
@@ -1,4 +0,0 @@
-from pickle import dump
-
-def salvar_classificador(modelo, nome_modelo):
-    dump(modelo, open(f'../models/Pima_{nome_modelo}.pkl', 'wb'))
